bf397bf5b69b93210aaf8b05dd1d29727cd61f0a
nosarzewski <nosarzewski.aleks@gmail.com>
2018-02-27 08:47:40 +0100

    short code added

---
+++ Class 1/Introduction_R.R
@@ -0,0 +1,61 @@
+# Statistical learning methods
+# Lab 1 - Introduction to R, Logistic regression, shiny, plotly
+# materials: https://github.com/nosarzewski/SGH_SDM_18
+# mail: nosarzewski.aleks@gmail.com, an56786@sgh.waw.pl
+
+# Plan
+#   I. About R
+#   II. Working R
+#   III. Objects in R
+#   IV. Data Export/Import 
+#   V. Statistics in R
+#   VI. Conditional instructions
+#   VII. Loops
+#   VIII. Functions
+
+# I. About R -------------------------------------------------------------
+
+# Where to find information?
+# http://www.r-project.org/doc/bib/R-books.html
+## manuals: 
+# http://cran.r-project.org/manuals.html
+## R for data analysis
+##  Gareth James, Daniela Witten, Trevor Hastie and Robert Tibshirani
+## An Introduction to Statistical Learning with Applications in R
+#http://www-bcf.usc.edu/~gareth/ISL/
+## Andy Field, Discovering Statistics Using R - very straightforward and easy to understand
+
+#Polish books, websites:
+## Bogumił Kamiński, Mateusz Zawisza "Receptury w R - Podręcznik dla ekonomistów"
+##K. Kopczewska, T. Kopczewski, P. Wójcik, Metody ilościowe w R. Aplikacje ekonomiczne i finansowe, 2009, CeDeWu
+## Eugeniusz Gatnar, Marek Walesiak "Analiza danych jakościowych i symbolicznych z wykorzystaniem programu R"
+## Eugeniusz Gatnar, Marek Walesiak "Statystyczna analiza danych z wykorzystaniem programu R"
+## Przemysław Biecek 'Przewodnik po pakiecie R'
+# http://www.biecek.pl/R/R.pdf
+## Przemysław Biecek 'Na przelaj przez Data Mining z pakietem R'
+# http://www.biecek.pl/NaPrzelajPrzezDataMining/NaPrzelajPrzezDataMining.pdf
+
+## Project website:
+# http://www.r-project.org/
+
+##Some MOOCs:
+# https://www.datacamp.com/courses/free-introduction-to-r
+# http://tryr.codeschool.com/
+# http://swirlstats.com/ (learning R in R!)
+# https://www.edx.org/course/introduction-r-data-science-microsoft-dat204x-7
+
+## I strongly recommend attending prof. Ramsza's classes (both in Polish and English) - a bit more demanding but very valueable:
+# Basic R programming / Podstawy programowania w R
+# http://michal.ramsza.org/
+
+# What if I did't find there help?
+# http://r.789695.n4.nabble.com/
+# http://stackoverflow.com/
+
+## Selected keyboard shortcuts in RSTUDIO
+# CTRL+ENTER: running a instruction (R GUI - F5): active line or 
+# TAB: hints for functions/objects
+# TAB after opening bracket: function arguments
+# F1 on function: help
+# CTRL+1: switching to editor
+# CTRL+2: switching to console
